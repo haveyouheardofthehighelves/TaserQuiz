e40d54c6cc5394229712cb3099628c0e41ef5a50
haveyouheardofthehighelves <32472487+haveyouheardofthehighelves@users.noreply.github.com>
2024-04-12 13:11:43 -0700

    added hardware functionality demo

---
+++ main.py
@@ -0,0 +1,17 @@
+import serial
+
+
+def writetoarduino(writeall):
+    arr = bytes(writeall, 'utf-8')
+
+
+def switch_light():
+    pass
+
+
+def button_press():
+    pass
+
+
+def activate_taser():
+    pass
